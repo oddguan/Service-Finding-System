dec7a4be87af8a1687ab142600823dfcbe181861
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 16:30:37 -0500

    sadgasg

--- connect.php
+++ connect.php
@@ -4,10 +4,10 @@ $username = "cguan3";
 $password = "bLzxj7r4";
 
 // create connection
-$conn = new mysqli($servername, $username, $password);
+$mysqli = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
+    die("Connection Failed: " . $mysqli->connect_error);
 }
 
-// echo "Connected Successfully.";
+echo "Connected Successfully.";
 ?>
