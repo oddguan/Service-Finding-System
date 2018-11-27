cd5a4a6dc5d48b051c730979074b1eea8e6de287
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 18:04:27 -0500

    sadfasd

--- view_order.php
+++ view_order.php
@@ -1,7 +1,11 @@
 <?php
     require_once("connect.php");
     $sql = "USE cguan3_1;";
-    $mysqli->query($sql);
+    if ($mysqli->query($sql) == TRUE){
+        echo "query succeeded.";
+    } else {
+        echo "query failed.";
+    }
     $sql = "SELECT * FROM Order_History;";
     if (!$result = $mysqli->query($sql)) {
         echo "Error" . "<br>\n";
