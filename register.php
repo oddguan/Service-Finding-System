d4a5c458d8cdf0acad5b800754f7869d036ebeb5
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-26 19:38:29 -0500

    test php

--- register.php
+++ register.php
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Register New Account</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="css/register.css" />
-</head>
-<body>
-    <?php
+<?php
     $sql = "USE cguan3_1;";
     require_once("connect.php");
     if ($conn->query($sql) === TRUE) {
-        echo "Database found";
+        // echo "Database found";
     }
     else {
         echo "Database not found";
     }
-    ?>
-</body>
-</html>
 
+    $input1 = $_POST("input1");
+    $input2 = $_POST("input2");
+    echo $input1;
+    echo $input2;
+    $sql.close();
+?>
