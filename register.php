86da7a7276532e222e59c28b0b3c204289ce3d45
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-26 19:40:28 -0500

    debug

--- register.php
+++ register.php
@@ -1,4 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
     $sql = "USE cguan3_1;";
     require_once("connect.php");
     if ($conn->query($sql) === TRUE) {
@@ -13,4 +24,6 @@
     echo $input1;
     echo $input2;
     $sql.close();
-?>
+    ?>
+</body>
+</html>
