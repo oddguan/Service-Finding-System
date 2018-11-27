bc24aeab2b46881224adee525f90b93d102ab4c8
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 17:52:10 -0500

    use database

--- view_order.php
+++ view_order.php
@@ -1,5 +1,7 @@
 <?php
     require_once("connect.php");
+    $sql = "USE cguan3_1;";
+    $mysqli->query($sql);
     $sql = "SELECT * FROM Order_History;";
     if (!$result = $mysqli->query($sql)) {
         echo "Error" . "<br>\n";
