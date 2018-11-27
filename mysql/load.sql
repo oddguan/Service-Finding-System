67456f352b30ead9d8c658988b44a00c25e8aace
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 17:10:56 -0500

    modified load script

--- mysql/load.sql
+++ mysql/load.sql
@@ -1,5 +1,4 @@
-LOAD DATA LOCAL INFILE "/home/cguan3/p1m3/data/reg_info.dat" INTO TABLE Registration FIELDS TERMINATED BY ",";
-LOAD DATA LOCAL INFILE "/home/cguan3/p1m3/data/supply.dat" INTO TABLE Supply FIELDS TERMINATED BY ",";
-LOAD DATA LOCAL INFILE "/home/cguan3/p1m3/data/demand.dat" INTO TABLE Demand FIELDS TERMINATED BY ",";
-LOAD DATA LOCAL INFILE "/home/cguan3/p1m3/data/order.dat" INTO TABLE Order_History FIELDS TERMINATED BY ",";
-
+LOAD DATA LOCAL INFILE "../data/reg_info.dat" INTO TABLE Registration FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "../data/supply.dat" INTO TABLE Supply FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "../data/demand.dat" INTO TABLE Demand FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "../data/order.dat" INTO TABLE Order_History FIELDS TERMINATED BY ",";
