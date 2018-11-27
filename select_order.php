f7d4d89a6f55dba981296de5b0ede4c42795951d
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 17:02:54 -0500

    wrapped with if

--- select_order.php
+++ select_order.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Show order</title>
+    <title>Select order</title>
 </head>
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
@@ -21,26 +21,24 @@
         echo"<br>\n";
         echo $mysqli->error . "\n";
     }
-    
-    $sql = "SELECT $orderID FROM Order_History;";    
-    if (!$result = $mysqli->query($sql)) {
-        // Oh no! The query failed. 
-        echo "Sorry, the website is experiencing problems.";
-    
-        // Again, do not do this on a public site, but we'll show you how
-        // to get the error information
-        echo "Error: Our query failed to execute and here is why: \n";
-        echo "Query: " . $sql . "\n";
-        echo "Errno: " . $mysqli->errno . "\n";
-        echo "Error: " . $mysqli->error . "\n";
-        exit;
+    if (!empty($orderID)) {
+        $sql = "SELECT $orderID FROM Order_History;";    
+        if (!$result = $mysqli->query($sql)) {
+            // Oh no! The query failed. 
+            echo "Sorry, the website is experiencing problems.";
+            echo "Error: Our query failed to execute and here is why: \n";
+            echo "Query: " . $sql . "\n";
+            echo "Errno: " . $mysqli->errno . "\n";
+            echo "Error: " . $mysqli->error . "\n";
+            exit;
+        }
+        else {
+            $arr = $result->fetch_assoc();
+            $selected = $arr[0];
+            echo $selected;
+        }
     }
-    else {
-        $arr = $result->fetch_assoc();
-        $selected = $arr[0];
-        echo $selected;
-    }
-
+    
     $mysqli->close();
     ?>
 </body>
