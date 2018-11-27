f8676b3c9be1c19b66000d4479481df0bbdfeb08
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 15:26:36 -0500

    test

--- register.php
+++ register.php
@@ -5,15 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
 </head>
 <body>
     <?php
     $sql = "USE cguan3_1;";
     require_once("connect.php");
     if ($conn->query($sql) === TRUE) {
-        // echo "Database found";
+        echo "Database found";
     }
     else {
         echo "Database not found";
