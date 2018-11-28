c8f54837fac7629b6714a62044f6a4b16b72c77f
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 20:03:22 -0500

    view order

--- view_Order.php
+++ view_Order.php
@@ -1,23 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
 <?php
-    require_once("connect.php");
-    $sql = "USE cguan3_1;";
-    $result = $mysqli->query($sql);
-    $sql = "SELECT * FROM Order_History;";
-    if (!$result = $mysqli->query($sql)) {
-        echo "Error" . "<br>\n";
-        echo $mysqli->error . "\n";
-    }
-    else {
-        echo "rows got:" . $result->num_rows . "<br><br>";
-        // $arr = $mysqli->fetch_assoc();
-        while ($row = $result->fetch_assoc()) {
-            echo $row['orderID'] . "<br>";
-            echo $row['supplier_account'] . "<br>";
-            echo $row['demander_account'] . "<br>";
-            echo $row['time_session'] . "<br>";
-            echo $row['service_type'] . "<br>";
-            echo $row['payment'] . "<br>";
-            echo "<br><br>";
-        }
-    }
+require_once("connect.php");
+$sql = "USE cguan3_1;";
+if ($mysqli->query($sql) === TRUE) {
+   // echo "using Database tbiswas2_company";
+} else {
+   echo "Error using  database: " . $mysqli->error;
+}
+// Query:
+$sql = "SELECT * FROM Order_History";
+$result = $mysqli->query($sql);
+if($result->num_rows > 0){
+
 ?>
+   <table class="table table-striped">
+      <tr>
+         <th>Order ID</th>
+         <th>Supplier Account</th>
+         <th>Demander Account</th>
+         <th>Time Session</th>
+         <th>Service Type</th>
+         <th>Payment</th>
+      </tr>
+<?php
+while($row = $result->fetch_assoc()){
+?>
+      <tr>
+          <td><?php echo $row['orderID']?></td>
+          <td><?php echo $row['supplier_account']?></td>
+          <td><?php echo $row['demander_account']?></td>
+          <td><?php echo $row['time_session']?></td>
+        <td><?php echo $row['service_type']?></td>
+        <td><?php echo $row['payment']?></td>
+      </tr>
+
+<?php
+}
+}
+else {
+echo "Nothing to display";
+}
+?>
+
+    </table>
+
+<?php
+$mysqli->close();
+?>
+
+</body>
+</html>
+
