d843c66ad65f385d2d427ee21df127007ee74cb5
Chenxiao Guan <1011zaozao@gmail.com>
2018-11-27 20:04:45 -0500

    changed path

--- mysql/load.sql
+++ mysql/load.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE "../data/reg_info.dat" INTO TABLE Registration FIELDS TERMINATED BY ",";
-LOAD DATA LOCAL INFILE "../data/supply.dat" INTO TABLE Supply FIELDS TERMINATED BY ",";
-LOAD DATA LOCAL INFILE "../data/demand.dat" INTO TABLE Demand FIELDS TERMINATED BY ",";
-LOAD DATA LOCAL INFILE "../data/order.dat" INTO TABLE Order_History FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "/home/cguan3/public_html/data/reg_info.dat" INTO TABLE Registration FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "/home/cguan3/public_html/data/supply.dat" INTO TABLE Supply FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "/home/cguan3/public_html/data/demand.dat" INTO TABLE Demand FIELDS TERMINATED BY ",";
+LOAD DATA LOCAL INFILE "/home/cguan3/public_html/data/order.dat" INTO TABLE Order_History FIELDS TERMINATED BY ",";
