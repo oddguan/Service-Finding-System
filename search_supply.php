783096941d7af1656025c1cc9361612f31c31046
Chenxiao Guan <1011zaozao@gmail.com>
2018-12-15 18:52:31 -0500

    random

---
+++ search_supply.php
@@ -0,0 +1,109 @@
+<?php   session_start();  ?>
+
+<html>
+    <head>Search Supply</head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <body>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        What kind of service are you providing? : 
+        <select name="service">
+            <option value="any">Any</option>
+            <option value="House_Cleaning">House Cleaning</option>
+            <option value="Lawn_Trimming">Lawn Trimming</option>
+            <option value="Babysitting">Babysitting</option>
+        </select>
+        <input type="submit" value="Search">
+    </form>
+
+    <?php
+    require_once("connect.php");
+    $service = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $service = $_POST['service'];
+    }
+    $sql = "USE cguan3_1;";
+    $result = $mysqli->query($sql);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_POST['service'] == "any") {
+            $sql = "SELECT * FROM Supply";
+        }
+        else {
+            $sql = "SELECT * FROM Supply WHERE service_type=\"" . $service .  "\";";
+        }
+        if (!$result = $mysqli->query($sql)) {
+            // Oh no! The query failed. 
+            echo "Sorry, the website is experiencing problems. <br>";
+        
+            // Again, do not do this on a public site, but we'll show you how
+            // to get the error information
+            echo "Error: Our query failed to execute and here is why: <br>\n";
+            echo "Query: " . $sql . "<br>\n";
+            echo "Errno: " . $mysqli->errno . "<br>\n";
+            echo "Error: " . $mysqli->error . "<br>\n";
+            exit;
+        }
+        else { ?>
+        <table class="table table-striped">
+            <tr>
+                <th>Account</th>
+                <th>Service Type</th>
+                <th>Salary Requirement</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th> </th>
+            </tr>
+
+        <?php
+            while($row = $result->fetch_assoc()) { ?>
+            <tr>
+                <td><?php echo $row['account']?></td>
+                <td><?php echo $row['service_type']?></td>
+                <td><?php echo $row['salary_requirement']?></td>
+	            <td><?php echo $row['start_time']?></td>
+	            <td><?php echo $row['end_time']?></td>
+                <td>
+                    <form method="get" action="submit.php">
+                    <button type="submit" name="select" value="<?= $row ?>">select</button>
+                    </form>
+                </td>
+            </tr>
+            
+            <?php
+            $_SESSION["select"] = $_GET["select"];
+            }
+        }
+    }
+
+    function select_input() {
+    
+        if(!empty($result)){
+            echo "if";
+            echo $result;
+            print_r($result);
+        }
+        else {
+            echo 'else';
+            $result = $_GET['select'];
+            echo $result;
+            $_SESSION['result'] = $result;
+        }
+        
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+    </table>
+    
+    
+    </body>
